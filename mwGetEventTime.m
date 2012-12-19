1621f912d8cf3c583cd97288ab68fb1c7b283ef8
Mark Histed <mhisted@gmail.com>
2012-12-18 19:57:49 -0500

    Fix subtle bugs in mwGetEventTime: note: 4th arg is 'value' not 'ignoreMissing'
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- mwGetEventTime.m
+++ mwGetEventTime.m
@@ -9,15 +9,16 @@ function time_us = mwGetEventTime(events, codec, tag, ...
 %  MH 100115: created
 %$Id$
 
-if nargin < 4 || isempty(occurrence), occurrence = 1; end
+if nargin < 4 || isempty(occurrence), occurrence = 'all'; end
 if nargin < 5 || isempty(value), value = []; end
 if nargin < 6, ignoreMissingStr = []; end
 
+% check ignoreMissingStr
 if isempty(ignoreMissingStr)
   doIgnoreMissing=false;
-elseif isnumeric(ignoreMissingStr) && ignoreMissingStr == 0
+elseif (isnumeric(ignoreMissingStr) || islogical(ignoreMissingStr)) && ignoreMissingStr == 0 
   doIgnoreMissing=false; 
-elseif isnumeric(ignoreMissingStr) && ignoreMissingStr == 1
+elseif (isnumeric(ignoreMissingStr) || islogical(ignoreMissingStr)) && ignoreMissingStr == 1
   doIgnoreMissing=true; 
 elseif ischar(ignoreMissingStr) && strcmpi(ignoreMissingStr, 'ignoremissing')
   doIgnoreMissing=true;
@@ -25,42 +26,57 @@ else
   error('Unknown ignoremissing value: %s', mat2str(ignoreMissingStr));
 end
 
+% check value
+if ischar(value)
+    if ~strcmpi(value, 'any')
+        error('value must be numeric or ''any''');
+    end
+end
+
+
 codes = [events.event_code];
 tCode = codec_tag2code(codec, tag);
-
 eventNs = find(codes == tCode);
+
 if length(eventNs) < 1
   if ~doIgnoreMissing
-    disp(sprintf('Code not found: %s', tag));
-    time_us = []; 
-    return
+    disp(sprintf('Code not found (in mwGetEventTime): %s', tag));
   end
+  time_us = []; 
+  return
 else
   % at least one
-  if ~isempty(value)
-    datas = [events(eventNs).data];
-    dataNs = find(datas == value);
-    tN = [];
-    if strcmpi(occurrence, 'all')
-      time_us = cat(2,events(eventNs).time_us);
-      return
-    elseif length(dataNs) == 0
-      disp(sprintf('Code %s with value %d not found', tag, value));
-    elseif length(dataNs) < occurrence
-      disp(sprintf('Asked for %d codes %s with value %d, but found %d with this value (%d total)', ...
-                   occurrence, tag, value, length(dataNs), length(eventNs) ));
-    else
-      tN = eventNs(dataNs(occurrence));
-    end
+  eVals = cat(2,events(eventNs).data);
+  eTs = cat(2,events(eventNs).time_us);
+
+  % do indexing based on value if desired
+  if isempty(value) || (ischar(value) && strcmpi(value) == 'any')
+      dataNs = 1:length(eVals);
+      valStr = 'any';
   else
-    % no value filtering
-    tN = eventNs(occurrence);
+      dataNs = find(eVals == value);
+      valStr = char(value);
+      if length(dataNs) == 0
+          disp(sprintf('Code %s with value %s not found', tag, valStr));
+          time_us = [];  
+          return
+      end
   end
-  if isempty(tN)
-    time_us = [];
-    return
+
+  if strcmpi(occurrence, 'all') || isempty(occurrence)
+      time_us = eTs(dataNs);
+      return
+  elseif strcmpi(occurrence, 'last')
+      time_us = eTs(dataNs(end));
+      return
+  elseif length(dataNs) < occurrence
+      disp(sprintf('Asked for %d codes %s with value %d, but found %d with this value (%d total)', ...
+                   occurrence, tag, value, length(dataNs), length(eventNs)));
+      time_us = [];
+      return
   else
-    time_us = events(tN).time_us;
+      time_us = eTs(dataNs(occurrence));
+      return
   end
 end
 
